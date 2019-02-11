b19a9b7ade6b8f9b1fd0d1d1e4016b645c4e0244
ajeeshklr <ajeeshklr@gmail.com>
2019-02-11 12:23:43 +0530

    Added license information

--- example/stringentry.dart
+++ example/stringentry.dart
@@ -1,5 +1,9 @@
 import 'package:mixed_collection/mixed_collection.dart';
 
+// Copyright (c) 2019, iMeshAcademy authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
 class StringCollectionEntry extends CollectionEntry<String> {
   StringCollectionEntry(String value) : super(value);
   @override
